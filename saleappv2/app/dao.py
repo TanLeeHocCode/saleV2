fd01e5bfa905ab9c83ad7e263707cd4cf0b1fdb6
w3schools-test <test@w3schools.com>
2023-11-07 16:56:23 +0700

    app

---
+++ saleappv2/app/dao.py
@@ -0,0 +1,14 @@
+from app.models import Category, Product
+
+
+def load_categories():
+    return Category.query.all()
+
+
+def load_products(kw):
+    products = Product.query
+
+    if kw:
+        products = products.filter(Product.name.contains(kw))
+
+    return products.all()
